f56b97587225aae8a190d80261f0af62965a7e64
sagar <skale4@binghamton.edu>
2017-11-03 01:01:40 -0500

    counter progress bar and UI - final

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/** Initial Loading **/
 var counter = 0;
 var upperLimit = 0;
 var counterElement = document.getElementById('counter');
@@ -6,46 +7,27 @@ var timer = null;
 var fingers = document.getElementById('fingers');
 var toes = document.getElementById('toes'); 
 var error = document.getElementById('error');
+var progressBar = document.getElementsByTagName("progress")[0];
 
-function reset(){
-  //alert("reset");
-  clearInterval(timer);
-  inputfield.value = "";
-  counter = 0;
-  upperLimit = 0;
-  counterElement.innerHTML = counter;
-  resetFingers();
-  resetToes();
-  hideError();
-}
-
-function restart(){
-  //alert("restart");
-  clearInterval(timer);
-  counter = 0;
-  counterElement.innerHTML = counter;
-  resetFingers();
-  resetToes();
-  hideError();
-  feezbuzz();
-}
+/** Highlight Fingers and Toes **/
 
 function hightLightFingers(){
-  fingers.style.color = "red";
+  fingers.style.backgroundColor = "rgba(159, 124, 104, 1)";
 }
 
 function hightLightToes(){
-  toes.style.color = "red";
+  toes.style.backgroundColor = "rgba(159, 124, 104, 1)";
 }
 
 function resetFingers(){
-  fingers.style.color = "black";
+  fingers.style.backgroundColor = "rgba(159, 124, 104, 0.2)";
 }
 
 function resetToes(){
-  toes.style.color = "black";
+  toes.style.backgroundColor = "rgba(159, 124, 104, 0.2)";
 }
 
+/** Feez Buzz Algorithm **/
 function feezbuzzUtil(num){
   resetFingers();
   resetToes();
@@ -73,11 +55,13 @@ function feezbuzz(){
     else{
       counter++;
       counterElement.innerHTML = counter;
+      progressBar.value = counter;
       feezbuzzUtil(counter);
     }
   },(1000));
 }
 
+/** Error Display **/
 function showError(){
   error.style.display = "block";
 }
@@ -86,13 +70,40 @@ function hideError(){
   error.style.display = "none";
 }
 
+/** Reset Counter and upper limit **/
+function reset(){
+  clearInterval(timer);
+  inputfield.value = "";
+  counter = 0;
+  upperLimit = 0;
+  progressBar.value = 0;
+  progressBar.max = 0;
+  counterElement.innerHTML = counter;
+  resetFingers();
+  resetToes();
+  hideError();
+}
+
+/** Restarts Counter **/
+function restart(){
+  clearInterval(timer);
+  counter = 0;
+  counterElement.innerHTML = counter;
+  progressBar.value = 0;
+  resetFingers();
+  resetToes();
+  hideError();
+  feezbuzz();
+}
+
+/** Updates UpperLimit and starts counter **/
 function play(){
   clearInterval(timer);
   var num = parseInt(inputfield.value);
   if(num > 0){
     hideError();
     upperLimit += num;
-    //alert(upperLimit);
+    progressBar.max = upperLimit;
     feezbuzz();
   }
   else{
